2bcd91b7b69262427e9fbbb27e825a75f10e47df
Abel de la Noval <ryuto1234yolo@gmail.com>
2026-05-17 19:57:11 -0400

    Implement client arrival handling in HappyComputingSimulator

--- src/simulation/simulator.py
+++ src/simulation/simulator.py
@@ -89,7 +89,75 @@ class HappyComputingSimulator:
                     self.handle_specialist_finish(event)
 
     def handle_client_arrival(self, event):
-        pass
+        # -------------------------
+        # Crear cliente
+        # -------------------------
+        client_id = self.next_client_id
+        self.next_client_id += 1
+
+        client = Client(
+            id=client_id,
+            arrival_time=self.clock,
+            service_type=self.random.tipo_servicio(),
+        )
+
+        self.clients[client.id] = client
+
+        print(
+            f"[{self.clock:.2f}] Cliente {client.id} llegó (tipo {client.service_type})"
+        )
+
+        # -------------------------
+
+        # Programar siguiente llegada
+        # -------------------------
+
+        next_arrival = self.clock + self.random.tiempo_entre_llegadas()
+
+        self.event_queue.push(
+            Event(time=next_arrival, event_type=EventType.CLIENT_ARRIVAL)
+        )
+
+        # -------------------------
+        # Buscar vendedor libre
+        # -------------------------
+
+        free_seller = None
+
+        for seller in self.sellers:
+            if not seller.busy:
+                free_seller = seller
+                break
+
+        # -------------------------
+        # Si hay vendedor
+        # -------------------------
+
+        if free_seller:
+            free_seller.busy = True
+            free_seller.current_client_id = client.id
+
+            client.seller_start_time = self.clock
+
+            finish_time = self.clock + self.random.tiempo_vendedor()
+
+            self.event_queue.push(
+                Event(
+                    time=finish_time,
+                    event_type=(EventType.SELLER_FINISH),
+                    client_id=client.id,
+                    employee_id=free_seller.id,
+                )
+            )
+
+        # -------------------------
+        # Si no hay vendedor
+        # -------------------------
+
+        else:
+            self.seller_queue.append(client.id)
+
+            print(f"Cliente {client.id} entra en cola vendedor")
 
     def handle_seller_finish(self, event):
         pass
