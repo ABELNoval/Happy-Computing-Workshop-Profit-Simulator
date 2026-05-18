85fe2bdd82cc636ff92b6098f22e12bf64ccbc32
Abel de la Noval <ryuto1234yolo@gmail.com>
2026-05-17 20:44:39 -0400

    Refactor EventQueue to include event counter in push and pop methods

--- src/simulation/event_queue.py
+++ src/simulation/event_queue.py
@@ -4,12 +4,18 @@ import heapq
 class EventQueue:
     def __init__(self):
         self.events = []
+        self.counter = 0
 
     def push(self, event):
-        heapq.heappush(self.events, (event.time, event))
+
+        heapq.heappush(self.events, (event.time, self.counter, event))
+
+        self.counter += 1
 
     def pop(self):
-        return heapq.heappop(self.events)[1]
+
+        return heapq.heappop(self.events)[2]
 
     def is_empty(self):
+
         return len(self.events) == 0
