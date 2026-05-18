7caf914639da84459bae2a53a3c62eb39e670443
Abel de la Noval <ryuto1234yolo@gmail.com>
2026-05-17 21:31:05 -0400

    Implement client service handling for technicians and specialists in HappyComputingSimulator

--- src/simulation/simulator.py
+++ src/simulation/simulator.py
@@ -170,7 +170,77 @@ class HappyComputingSimulator:
 
         print(f"[{self.clock:.2f}] Cliente {client.id} terminó vendedor")
 
-        # tipo 4 sale
+        # -------------------------
+        # FLUJO CLIENTE
+        # -------------------------
+
+        # Tipo 1 y 2 → técnico
+        if client.service_type in [1, 2]:
+            free_technician = None
+
+            for technician in self.technicians:
+                if not technician.busy:
+                    free_technician = technician
+                    break
+
+            # técnico libre
+            if free_technician:
+                free_technician.busy = True
+                free_technician.current_client_id = client.id
+
+                client.technician_start_time = self.clock
+
+                finish_time = self.clock + self.random.tiempo_tecnico()
+
+                self.event_queue.push(
+                    Event(
+                        time=finish_time,
+                        event_type=(EventType.TECHNICIAN_FINISH),
+                        client_id=client.id,
+                        employee_id=(free_technician.id),
+                    )
+                )
+
+                print(f"Cliente {client.id} entra a técnico {free_technician.id}")
+
+            # técnico ocupado
+            else:
+                self.technician_queue.append(client.id)
+
+                print(f"Cliente {client.id} entra cola técnico")
+
+        # -------------------------
+        # Tipo 3 → especialista
+        # -------------------------
+
+        elif client.service_type == 3:
+            if not self.specialist.busy:
+                self.specialist.busy = True
+                self.specialist.current_client_id = client.id
+
+                client.specialist_start_time = self.clock
+
+                finish_time = self.clock + self.random.tiempo_tecnico_especializado()
+
+                self.event_queue.push(
+                    Event(
+                        time=finish_time,
+                        event_type=(EventType.SPECIALIST_FINISH),
+                        client_id=client.id,
+                        employee_id=1,
+                    )
+                )
+
+                print(f"Cliente {client.id} entra especialista")
+
+            else:
+                self.specialist_queue.append(client.id)
+
+                print(f"Cliente {client.id} entra cola especialista")
+
+        # -------------------------
+        # Tipo 4 → sale
+        # -------------------------
         if client.service_type == 4:
             client.exit_time = self.clock
 
@@ -215,7 +285,112 @@ class HappyComputingSimulator:
             seller.current_client_id = None
 
     def handle_technician_finish(self, event):
-        pass
+        technician = self.technicians[event.employee_id - 1]
+        client = self.clients[event.client_id]
+        client.technician_end_time = self.clock
+
+        print(f"[{self.clock:.2f}] Cliente {client.id} terminó técnico")
+
+        # -------------------------
+        # Cliente sale sistema
+        # -------------------------
+        client.exit_time = self.clock
+
+        # TODO:
+        # reemplazar por valores reales
+        if client.service_type == 1:
+            self.money += 0
+
+        elif client.service_type == 2:
+            self.money += 350
+
+        print(f"Cliente {client.id} salió")
+
+        # -------------------------
+        # Revisar cola técnico
+        # -------------------------
+
+        if len(self.technician_queue) > 0:
+            next_client_id = self.technician_queue.popleft()
+
+            next_client = self.clients[next_client_id]
+
+            technician.busy = True
+
+            technician.current_client_id = next_client.id
+
+            next_client.technician_start_time = self.clock
+
+            finish_time = self.clock + self.random.tiempo_tecnico()
+
+            self.event_queue.push(
+                Event(
+                    time=finish_time,
+                    event_type=(EventType.TECHNICIAN_FINISH),
+                    client_id=next_client.id,
+                    employee_id=(technician.id),
+                )
+            )
+
+            print(
+                f"Cliente {next_client.id} sale cola técnico → técnico {technician.id}"
+            )
+
+        else:
+            technician.busy = False
+            technician.current_client_id = None
 
     def handle_specialist_finish(self, event):
-        pass
+        client = self.clients[event.client_id]
+        client.specialist_end_time = self.clock
+
+        print(f"[{self.clock:.2f}] Cliente {client.id} terminó especialista")
+
+        # -------------------------
+        # Cliente sale sistema
+        # -------------------------
+
+        client.exit_time = self.clock
+
+        # tipo 3:
+        # cambio de equipo
+        self.money += 500
+
+        print(f"Cliente {client.id} salió")
+
+        # -------------------------
+        # Revisar cola especialista
+        # -------------------------
+
+        if len(self.specialist_queue) > 0:
+            next_client_id = self.specialist_queue.popleft()
+
+            next_client = self.clients[next_client_id]
+
+            self.specialist.busy = True
+
+            self.specialist.current_client_id = next_client.id
+
+            next_client.specialist_start_time = self.clock
+
+            finish_time = self.clock + self.random.tiempo_tecnico_especializado()
+
+            self.event_queue.push(
+                Event(
+                    time=finish_time,
+                    event_type=(EventType.SPECIALIST_FINISH),
+                    client_id=next_client.id,
+                    employee_id=1,
+                )
+            )
+
+            print(f"Cliente {next_client.id} sale cola especialista")
+
+        # -------------------------
+        # No hay cola
+        # -------------------------
+
+        else:
+            self.specialist.busy = False
+
+            self.specialist.current_client_id = None
