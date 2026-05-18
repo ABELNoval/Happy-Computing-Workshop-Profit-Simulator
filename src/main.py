70cb4da734fc8e17f415ce3dd221db2feaaa1320
Abel de la Noval <ryuto1234yolo@gmail.com>
2026-05-17 21:37:05 -0400

    Refactor main function to initialize HappyComputingSimulator with simulation time and enhance output formatting for results

--- src/main.py
+++ src/main.py
@@ -1,12 +1,39 @@
+import random
+
 from simulation.simulator import HappyComputingSimulator
 
 
 def main():
-    sim = HappyComputingSimulator()
+
+    sim = HappyComputingSimulator(simulation_time=60)
+
+    # -------------------------
+    # TEST CONTROLADO
+    # -------------------------
+
+    # mezcla de tipos
+    sim.random.tipo_servicio = lambda: random.choice([1, 2, 3, 4])
+
+    # llega un cliente cada 3 min
+    # (más legible)
+    sim.random.tiempo_entre_llegadas = lambda: 3
+
+    # vendedor rápido
+    sim.random.tiempo_vendedor = lambda: 5
+
+    # técnico
+    sim.random.tiempo_tecnico = lambda: 8
+
+    # especialista
+    sim.random.tiempo_tecnico_especializado = lambda: 12
 
     sim.run()
 
-    print("Simulación terminada")
+    print("\n========== RESULTADOS ==========")
+
+    print(f"Clientes creados: {len(sim.clients)}")
+
+    print(f"Dinero generado: {sim.money}")
 
 
 if __name__ == "__main__":
